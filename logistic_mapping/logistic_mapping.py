a5170cae628e64229aa2cf35f8271a23a74ad15f
t4t5u0 <ymmtryk0902@gmail.com>
2020-04-25 01:08:04 +0900

    画像が保存される場所を変更

--- logistic_mapping/logistic_mapping.py
+++ logistic_mapping/logistic_mapping.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 # x_n+1 = ax_n(1-x_n)
 # 初期条件
-a = 3.2
+a = 3.853
 x_0 = 0.1
 
 # logistic_mapping
@@ -23,14 +23,14 @@ x = logistic(a, x_0)
 fig_1 = plt.figure()
 ax_1 = fig_1.add_subplot(xlabel='x_n', ylabel='x_n+1', title='return map')
 ax_1.plot(x[:len(x)-1], x[1:],ls='', marker='.')
-fig_1.savefig(f'return_map_{a}_{x_0}.png')
+fig_1.savefig(f'./logistic_mapping/return_map_{a}_{x_0}.png')
 #plt.show()
 
 # 時系列のグラフ
 fig_2 = plt.figure()
 ax_2 = fig_2.add_subplot(xlabel='n', ylabel='x_n', title='time_sereis')
 ax_2.plot(x)
-fig_2.savefig(f'time_series_{a}_{x_0}.png')
+fig_2.savefig(f'./logistic_mapping/time_series_{a}_{x_0}.png')
 #plt.show()
 
 print('finish successfully')
